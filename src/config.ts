561c9b804633882b247bb0f625a6e7655423af3c
Kevin BIOJOUT <biojoutk1106@gmail.com>
2026-06-08 23:03:23 +0200

    bump siri ratelimit to 1/60s

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const PORT = 3000;
 export const REQUESTOR_REF = "opendata";
 export const SIRI_ENDPOINT =
 	"https://api.okina.fr/gateway/nam/realtime/anshar/ws/services/?datasetId=CA_LIMOGES_METROPOLE";
-export const SIRI_RATELIMIT = Temporal.Duration.from({ seconds: 30 }).total("milliseconds");
+export const SIRI_RATELIMIT = Temporal.Duration.from({ seconds: 60 }).total("milliseconds");
 export const SWEEP_THRESHOLD = Temporal.Duration.from({ minutes: 10 }).total("milliseconds");
